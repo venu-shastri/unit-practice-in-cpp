7de00031e6f18bf7e04d595c57f841ef8c7e38c3
Venugopal K <venu.shastri@gmail.com>
2024-11-21 23:29:30 +0530

    Update SpeedMonitor.h

--- SpeedMonitor.h
+++ SpeedMonitor.h
@@ -1,14 +1,18 @@
 #include "ILogger.h"
 #include "ISpeedSensor.h"
+#include "ICloudCommunicator.h"
 class SpeedMonitor{
     private : int _speedThreshold;
                 //Dependency
                 ILogger *_logger;
                 ISpeedSensor *_speedSensorInstance;
+                ICloudCommunicator *_cloudCommunicatorInstance;
   
       public:
-          SpeedMonitor(int speedThreshold,ILogger *logger,ISpeedSensor *speedSensor): _speedThreshold{speedThreshold},
+          SpeedMonitor(int speedThreshold,ILogger *logger,ISpeedSensor *speedSensor,ICloudCommunicator *cloudCommunicatorInstance):
+                                                                                    _speedThreshold{speedThreshold},
                                                                                      _logger{logger},
-                                                                                    _speedSensorInstance{speedSensorInstance} {}
+                                                                                    _speedSensorInstance{speedSensorInstance} 
+                                                                                    _cloudCommunicatorInstance{cloudCommunicatorInstance}{}
           void monitor();
 };
