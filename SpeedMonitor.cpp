1c12efc5cbeac8f4a62e4a9015e76ceba262c69a
Venugopal K <venu.shastri@gmail.com>
2024-11-21 23:15:03 +0530

    Update SpeedMonitor.cpp

--- SpeedMonitor.cpp
+++ SpeedMonitor.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
  void SpeedMonitor::monitor(){
             if(_speedThreshold  < 1 || _speedThreshold > 100){
-              this->_logger.write("_speedThreshold value must be in the ramge {1-100} "+ _speedThreshold");
+              this->_logger.write("_speedThreshold value must be in the ramge {1-100} "+ to_string(_speedThreshold)");
             }
               
             BNFSpeedSensor speedSensorInstance;
             IOTCloudCommunicator cloudCommuniccator;
             int currentSpeedInKmph=speedSensorInstance.getCurrentSpeed();
             
-            this._logger->write("Current Speed In Kmph " + currentSpeedInKmph);
+            this._logger->write("Current Speed In Kmph " + to_string(currentSpeedInKmph));
             if(currentSpeedInKmph > _speedThreshold){
                   double mph = currentSpeedInKmph * 0.621371;
                   string message="Current Speed in Miles "+ to_string(mph);
