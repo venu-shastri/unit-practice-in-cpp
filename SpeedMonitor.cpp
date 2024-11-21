355d799932e48b0e4ad06b34b60655334ddc95c3
Venugopal K <venu.shastri@gmail.com>
2024-11-21 23:09:46 +0530

    Update SpeedMonitor.cpp

--- SpeedMonitor.cpp
+++ SpeedMonitor.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
  void SpeedMonitor::monitor(){
             if(_speedThreshold  < 1 || _speedThreshold > 100){
-              cout<<"_speedThreshold value must be in the ramge {1-100} "<<_speedThreshold<<endl;
+              this->_logger.write("_speedThreshold value must be in the ramge {1-100} "+ _speedThreshold");
             }
               
             BNFSpeedSensor speedSensorInstance;
             IOTCloudCommunicator cloudCommuniccator;
             int currentSpeedInKmph=speedSensorInstance.getCurrentSpeed();
             
-            cout<<"Current Speed In Kmph "<<currentSpeedInKmph<<endl;
+            this._logger->write("Current Speed In Kmph " + currentSpeedInKmph);
             if(currentSpeedInKmph > _speedThreshold){
                   double mph = currentSpeedInKmph * 0.621371;
                   string message="Current Speed in Miles "+ to_string(mph);
                  int statusCode= cloudCommuniccator.pushMessage(message);
                   if(statusCode > 400){
                       //Log Message to Console
-                       cout<<"Error In Communication Unable to Contact Server "<<message<< endl;
+                       this->_logger.write("Error In Communication Unable to Contact Server " + message;
                   }
                  
             }
