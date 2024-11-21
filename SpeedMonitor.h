cab26163d9a43bd52c35b395b9225ae5e08e848d
Venugopal K <venu.shastri@gmail.com>
2024-11-21 23:18:31 +0530

    Update SpeedMonitor.h

--- SpeedMonitor.h
+++ SpeedMonitor.h
@@ -1,10 +1,14 @@
 #include "ILogger.h"
+#include "ISpeedSensor.h"
 class SpeedMonitor{
     private : int _speedThreshold;
                 //Dependency
                 ILogger *_logger;
+                ISpeedSensor *_speedSensorInstance;
   
       public:
-          SpeedMonitor(int speedThreshold,ILogger *logger): _speedThreshold{speedThreshold},_logger{logger} {}
+          SpeedMonitor(int speedThreshold,ILogger *logger,ISpeedSensor *speedSensor): _speedThreshold{speedThreshold},
+                                                                                     _logger{logger},
+                                                                                    _speedSensorInstance{speedSensorInstance} {}
           void monitor();
 };
