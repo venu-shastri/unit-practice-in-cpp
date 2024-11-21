3e34953c737a5b220701096013df6d8af9e0ab36
Venugopal K <venu.shastri@gmail.com>
2024-11-21 22:37:32 +0530

    Update SpeedMonitor.cpp

--- SpeedMonitor.cpp
+++ SpeedMonitor.cpp
@@ -3,28 +3,8 @@
 #include <string>
 using namespace std;
 
-class IOTCloudCommunicator{
-    public:
-          int  pushMessage(string message){
-              //Interact with internet and push the message to MessageQueue
-            //if communiction succesfull return value from ramge {200 -400} status code otherwise returns errocode (400 - 500)
-              std::random_device rd;
-                std::mt19937 gen(rd());
-                std::uniform_int_distribution<> distr(200, 500);
-                return distr(gen);
-            
-          }
 
-};
-class BNFSpeedSensor{
-        public:
-            int   getCurrentSpeed(){
-                std::random_device rd;
-                std::mt19937 gen(rd());
-                std::uniform_int_distribution<> distr(1, 100);
-                return distr(gen);
-            }
-};
+
 
 class SpeedMonitor{
     private : int _speedThreshold;
