e6f00ef16c015258d14f3171d6dc76fead7284fb
Venugopal K <venu.shastri@gmail.com>
2024-11-21 23:11:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "SpeedMonitor.h"
+#include "ConsoleLogger.h"
 int main(){
-SpeedMonitor instance {10};
+  ConsoleLogger logger;
+SpeedMonitor instance {10,&logger};
   instance.monitor();
   instance.monitor();
   instance.monitor();
